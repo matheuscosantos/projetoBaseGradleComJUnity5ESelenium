8cebead2e9dfd6a579fefb915da67797677b8609
matheuscosantos <42250854+matheuscosantos@users.noreply.github.com>
2019-10-16 23:30:16 -0300

    Add: @BeforeEach e @AfterEach

--- src/test/java/TesteWebDriver.java
+++ src/test/java/TesteWebDriver.java
@@ -1,21 +1,40 @@
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TesteWebDriver {
 
-    @Test
-    public void testaNavegador() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+    private WebDriver driver;
+    private WebElement searchBox;
+    private WebElement searchResults;
 
-        driver.manage().window().maximize();
-        driver.get("http://www.google.com.br");
-        TimeUnit.SECONDS.sleep(5);
-        driver.get("https://github.com/matheuscosantos");
-        TimeUnit.SECONDS.sleep(5);
+    @BeforeEach
+    public void abreNavegador(){
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void fechaNavegador(){
         driver.close();
     }
+
+    @Test
+    public void verificaTitulo() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com.br");
+        TimeUnit.SECONDS.sleep(3);
+        driver.get("https://github.com/matheuscosantos");
+        TimeUnit.SECONDS.sleep(3);
+        assertEquals(driver.getTitle(), "matheuscosantos (matheuscosantos) \u00b7 GitHub");
+    }
 }
+
